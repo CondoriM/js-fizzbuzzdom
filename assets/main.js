da9c2750f2df4aae220e613e28af2e8e67418dec
CondoriM <condoria.miguel@gmail.com>
2022-03-28 16:20:28 +0200

    completed fizz and buzz

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,26 @@
-alert("ciao")
+const myLista = document.getElementById('lista');
+
+for ( i = 1; i <= 100; i++){
+    
+    if (i % 3 == 0){
+        const listItem = document.createElement('li');
+        listItem.innerHTML = 'Fizz';
+
+        myLista.appendChild(listItem);
+    } else if (i % 5 == 0){
+        const listItem = document.createElement('li');
+        listItem.innerHTML = 'Buzz';
+
+        myLista.appendChild(listItem);
+    } else if (i % 5 == 0, i % 3 == 0){
+        const listItem = document.createElement('li');
+        listItem.innerHTML = 'FizzBuzz';
+
+        myLista.appendChild(listItem);
+    } else {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${i}`;
+
+        myLista.appendChild(listItem);
+    }
+}
